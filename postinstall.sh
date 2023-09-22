ade2e8f090ea64ce346fcc76ba558a7447d154f4
pdhoward <patrick.howard@hotmail.com>
2023-09-22 13:24:00 -0500

    structure import for a postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,10 @@
 # Copy app from node_modules to root
 cp -R node_modules/migrate/* ./
 
+# Commands below not needed because Stackblitz handles transitive dependencies
+
 # Navigate to root (for clarity, in case the script's context isn't already root)
 cd ..
 
 # Install dependencies of the copied app
-npm install
+# npm install
